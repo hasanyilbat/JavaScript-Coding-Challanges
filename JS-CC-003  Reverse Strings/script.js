2333c70cb460c439cfacd505fe79b1735ceedfaa
Hasan <hasan.yilbat@gmail.com>
2022-05-19 20:08:22 +0300

    Reverse Strings

---
+++ JS-CC-003  Reverse Strings/script.js
@@ -0,0 +1,18 @@
+//! *******************************************
+//* const text = "Clarusway Rocks!";
+//* let reverseText = (text.split("").reverse().join(""));
+//* console.log(reverseText);
+//! *******************************************
+
+//! *******************************************
+//* const text = "Clarusway Rocks!";
+//* let arrayList = [];
+//*let reverseText = text.split("");
+//*let son;
+//* for (let i = reverseText.length - 1; i >= 0; i--) {
+//*   const element = reverseText[i];
+//*   arrayList.push(element);
+//*   son = arrayList.join("");
+//* }
+//* console.log(son);
+//! *******************************************
